74042c67d7e67c7b45fb73af79d1c59ff58e8e57
Jalda Dehghan <Hillie92@hotmail.com>
2016-11-11 00:10:57 +0100

    Add files via upload

---
+++ mjau.py
@@ -0,0 +1,8 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name=input("Vad heter du?")
+Vad heter du?
+>>> print("hej", name)
+hej 
+>>> if name=="Lisa":
+	print("Hej programmerare!")
---
+++ padi.py
@@ -0,0 +1,4 @@
+from turtle import *
+for n in range(0,5):
+    forward(200)
+    right(144)
